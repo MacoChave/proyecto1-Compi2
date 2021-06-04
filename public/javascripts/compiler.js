93e2d93aa6be56bfa876556cbbfac10527ab0736
Marco Chávez <catalantt@gmail.com>
2021-06-04 09:08:24 -0600

    Campo para comando xpath agregado en editor

--- public/javascripts/compiler.js
+++ public/javascripts/compiler.js
@@ -1,7 +1,13 @@
 "use strict";
 analizeCode.addEventListener('click', () => {
+    // ANALIZAR XML
     console.log(codeBlock.textContent);
 });
+sendXPath.addEventListener('click', () => {
+    // ANALIZAR XPATH
+    console.log(codeXPath.value);
+});
 compileCode.addEventListener('click', () => {
+    // COMPILAR XPATH
     console.log('compileCode was clicked');
 });
--- ts/compiler.ts
+++ ts/compiler.ts
@@ -1,7 +1,14 @@
 analizeCode.addEventListener('click', () => {
+	// ANALIZAR XML
 	console.log(codeBlock.textContent);
 });
 
+sendXPath.addEventListener('click', () => {
+	// ANALIZAR XPATH
+	console.log(codeXPath.value);
+});
+
 compileCode.addEventListener('click', () => {
+	// COMPILAR XPATH
 	console.log('compileCode was clicked');
 });
